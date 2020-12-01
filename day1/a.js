bac367d250b54ef15ad9b4f54da53a277fc24906
OliverEkberg <oliver.ekberg1889@gmail.com>
2020-12-01 22:50:31 +0100

    Day 1 - A refactor

--- day1/a.js
+++ day1/a.js
@@ -1,9 +1,10 @@
 const data = require('./a.data')
 const foundValues = new Set()
+const desiredSum = 2020
 
 for (const val of data) {
-  if (foundValues.has(2020 - val)) {
-    console.log(val, 2020 - val, val * (2020 - val))
+  if (foundValues.has(desiredSum - val)) {
+    console.log(val, desiredSum - val, val * (desiredSum - val))
     break
   }
   foundValues.add(val)
