0f9e435df86ba75776a325e7c1a424d3cfa98b9a
OliverEkberg <oliver.ekberg1889@gmail.com>
2020-12-25 22:09:43 +0100

    Day 16 - Lint

--- day16/b.js
+++ day16/b.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const [rulesSection, [, myTicket], nearbyTicketsSection] = fs.readFileSync('data.txt', 'utf8')
-.split('\n\n')
-.map(section => section.split('\n'))
+  .split('\n\n')
+  .map(section => section.split('\n'))
 
 const fieldRx = /(.+): /
 const rangeRx = / ([0-9]+)-([0-9]+)/g
@@ -14,12 +14,12 @@ const ruleMap = new Map()
 for (const rule of rulesSection) {
   const [, field] = rule.match(fieldRx)
   const okValues = new Set()
-  
+
   const rangeMatches = rule.matchAll(rangeRx)
   for (const [, min, max] of rangeMatches) {
     for (let k = Number(min); k <= Number(max); k++) okValues.add(k)
   }
-  
+
   ruleMap.set(field, okValues)
 }
 
@@ -38,20 +38,20 @@ const getPassingRules = (value) => {
       passing.push(rule)
     }
   }
-  
+
   return passing
 }
 
 const matchesAnyRule = value => getPassingRules(value).length > 0
 
 const validTickets = nearbyTickets
-.map(ticket => ticket.split(',').map(Number))
-.filter(ticket => ticket.every(matchesAnyRule))
+  .map(ticket => ticket.split(',').map(Number))
+  .filter(ticket => ticket.every(matchesAnyRule))
 
 const columns = []
 for (let col = 0; col < validTickets[0].length; col++) {
   columns.push([])
-  
+
   for (let row = 0; row < validTickets.length; row++) {
     columns[col].push(validTickets[row][col])
   }
@@ -62,7 +62,7 @@ const ruleColumnsMap = new Map()
 for (let i = 0; i < columns.length; i++) {
   const column = columns[i]
   let passingRules = new Set()
-  
+
   for (const value of column) {
     if (passingRules.size === 0) {
       passingRules = new Set(getPassingRules(value))
@@ -71,12 +71,12 @@ for (let i = 0; i < columns.length; i++) {
       passingRules = intersect(passingRules, currentPassingRules)
     }
   }
-  
+
   for (const passingRule of passingRules) {
     if (!ruleColumnsMap.has(passingRule)) {
       ruleColumnsMap.set(passingRule, new Set())
     }
-    
+
     ruleColumnsMap.get(passingRule).add(i)
   }
 }
@@ -86,7 +86,7 @@ const finalRuleColumnMap = new Map()
 while (ruleColumnsMap.size) {
   let rule
   let columnId
-  
+
   for (const [currentRule, columnIds] of ruleColumnsMap) {
     if (columnIds.size === 1) {
       rule = currentRule
@@ -94,14 +94,14 @@ while (ruleColumnsMap.size) {
       break
     }
   }
-  
+
   ruleColumnsMap.delete(rule)
-  
+
   // Remove columnId from as potential match for other columns
   for (const [, columnIds] of ruleColumnsMap) {
     columnIds.delete(columnId)
   }
-  
+
   finalRuleColumnMap.set(rule, columnId)
 }
 
